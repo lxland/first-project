d6d9cbf7f8d7cd46b6dc667ef80355d215fdd187
zhang <zlx_summer@163.com>
2017-07-05 16:51:06 +0800

    js

---
+++ static/js/uploadFile.js
@@ -0,0 +1,19 @@
+$(function(){
+	$('#files').change(function(){
+		var _t = $(this);
+		var fileName = _t.val();
+		var fileSize = document.getElementById("files").files;
+
+		// 判断文件格式
+		if( !/.(jpg|jpeg|png|JPG)$/.test(fileName) ){
+			alert("图片类型必须是jpeg,jpg,png中的一种");
+		} else if( fileSize[0].size > 1024*1024 ){
+			alert('请上传小于1M的照片');
+		}
+
+		
+	})
+})
+
+// 图片懒加载
+$("img.lazy").lazyload({effect: "fadeIn"});
