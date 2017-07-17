5e25ea9f38cb956bdc6b48281576a1858fb87866
zhang <zlx_summer@163.com>
2017-07-17 10:18:32 +0800

    html

--- static/js/uploadFile.js
+++ static/js/uploadFile.js
@@ -31,25 +31,30 @@ $(function(){
 })
 
 // 图片懒加载
-$("img.lazy").lazyload({effect: "fadeIn"});
+// $("img.lazy").lazyload({effect: "fadeIn"});
 
+$("[data-toggle='tooltip']").tooltip();
 
 // 瀑布流
-	$('#lightBoxMain').waterfall({
-	    itemClass: "#lightBoxMain li",
-	    minColCount: 2,
-	    spacingHeight: 10,
-	    resizeable: true,
-	    ajaxCallback: function(success, end) {
-	        var data = {"data": [
-	            { "original": "1-1.jpg" }, { "original": "2-3.jpg" }, { "original": "2-4.jpg" }, { "original": "2-5.jpg" }, { "original": "2-6.jpg" }
-	        ]};
-	        var str = "";
-	        var templ = '<li style="opacity:0;filter:alpha(opacity=0);"><img class="lazy" data-original="static/images/{{original}}" /></li>'
+$('#lightBoxMain').waterfall({
+    itemClass: "#lightBoxMain li",
+    minColCount: 2,
+    spacingHeight: 10,
+    resizeable: true,
+    ajaxCallback: function(success, end) {
+        var data = {"data": [
+            { "original": "1-1.jpg" }, { "original": "2-3.jpg" }, { "original": "2-4.jpg" }, { "original": "2-5.jpg" }, { "original": "2-6.jpg" }
+        ]};
+        var str = "";
+        var templ = '<li style="opacity:0;filter:alpha(opacity=0);"><img class="lazy" src="static/images/{{original}}" alt="" /><div class="cover-stat"><i class="fa fa-eye"></i><span class="pr10"> &nbsp;10000</span><div class="cover-yh"><a href="#" data-toggle="tooltip" data-placement="top" title="jack"><i class="fa fa-user-secret"></i></a></div></div></li>';
 
-	        for(var i = 0; i < data.data.length; i++) {
-	            str += templ.replace("{{original}}", data.data[i].original);
-	        }
-	        $(str).appendTo($('#lightBoxMain'));
-	    }
-	});
+        for(var i = 0; i < data.data.length; i++) {
+            str += templ.replace("{{original}}", data.data[i].original);
+        }
+        $(str).appendTo($('#lightBoxMain'));
+        $("[data-toggle='tooltip']").tooltip();
+        
+        success();
+	    end();
+    }
+});
